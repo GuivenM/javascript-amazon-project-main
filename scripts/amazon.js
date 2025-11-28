101071cf2a428a549986379430ebe2be8bd6cb7c
Guiven <guivenpambou@gmail.com>
2025-11-28 17:02:31 +0100

    add product by quantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-product-quantity-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -60,7 +60,8 @@ document.querySelector('.js-products-grid')
 document.querySelectorAll('.js-add-to-cart')
     .forEach((button) => {
         button.addEventListener('click', () => {
-           const productId = button.dataset.productId;
+          const productId = button.dataset.productId;
+          quantity = Number(document.querySelector(`.js-product-quantity-${productId}`).value);
 
            let matchingItem;
 
@@ -71,11 +72,11 @@ document.querySelectorAll('.js-add-to-cart')
            });
 
            if (matchingItem) {
-            matchingItem.quantity += 1;
+            matchingItem.quantity += quantity;
            } else {
             cart.push({
               productId,
-              quantity: 1
+              quantity
             });
            }
 
